4ba8543429d6b62d3586d2087c96afe88f6f4aac
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-11-24 16:37:53 +0000

    Initial Commit

---
+++ src/main/java/com/group19/reportingservice/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.group19.reportingservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping("/hello")
+    public String sayHi() {
+        return "Hello World";
+    }
+}
---
+++ src/test/java/com/group19/reportingservice/ReportingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group19.reportingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReportingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
