7e72e39810de9a7f7061cf4b989514f298c5d04f
ofoe-fiergbor <ofoefiergbor16@gmail.com>
2021-12-13 10:42:25 +0000

    removed firebase config

--- src/main/java/com/group19/reportingservice/ReportingServiceApplication.java
+++ src/main/java/com/group19/reportingservice/ReportingServiceApplication.java
@@ -1,14 +1,10 @@
 package com.group19.reportingservice;
 
-import com.group19.reportingservice.model.dto.MessageDto;
-import com.group19.reportingservice.service.firebase.MessagingService;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-import java.util.concurrent.ExecutionException;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -18,8 +14,4 @@ public class ReportingServiceApplication {
 		SpringApplication.run(ReportingServiceApplication.class, args);
 	}
 
-	@Bean
-	public List<MessageDto> messaging() throws ExecutionException, InterruptedException {
-		return new MessagingService().getMessages();
-	}
 }
